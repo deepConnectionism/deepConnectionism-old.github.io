2d67b07d19c1b2cfbe0037004c1cbe32a3dd7baf
Hymnhyz <904964313@qq.com>
2022-07-24 02:55:07 +0800

    统计

--- assets/js/tongji.baidu.js
+++ assets/js/tongji.baidu.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ec7470452f344e57c8085be5be58583f";
-  var s = document.getElementsByTagName("script")[0];
+  hm.src = "https://hm.baidu.com/hm.js?255a329b2e02f2697932c2fcb857990b";
+  var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
